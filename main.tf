85ad249f557dccce46ec785250174eb64abb3559
Param <param135@gmail.com>
2020-11-10 17:01:33 -0500

    Modularised

--- main.tf
+++ main.tf
@@ -18,40 +18,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_vpc" "my_new_vpc" {
-  cidr_block = "10.0.0.0/16"
+module "my_new_alpine_ec2_instance" {
+  source = "./modules/new_alpine_ec2_instance"
 
-  tags = {
-    Name = "my_new_vpc"
+  ec2_tags = {
+    Name = "my_new_alpine_ec2_instance"
   }
 }
-
-resource "aws_security_group" "my_new_group" {
-  name   = "my_new_group"
-  vpc_id = aws_vpc.my_new_vpc.id
-
-  tags = {
-    Name = "my_new_group"
-  }
-}
-
-resource "aws_subnet" "my_new_subnet" {
-  vpc_id     = aws_vpc.my_new_vpc.id
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "my_new_subnet"
-  }
-}
-
-resource "aws_instance" "my_new_ec2_instance" {
-  ami                    = "ami-0947d2ba12ee1ff75"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.my_new_subnet.id
-  vpc_security_group_ids = [aws_security_group.my_new_group.id]
-
-  tags = {
-    Name = "my_new_ec2_instance"
-  }
-}
-
