ecbf7cf46edc8bef05cbffc9850aef34389d7027
Param <param135@gmail.com>
2020-11-10 10:40:02 -0500

    post saving and contacting Edvin

--- main.tf
+++ main.tf
@@ -9,5 +9,14 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
+
+resource "aws_vpc" "my_new_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my_new_vpc"
+  }
+}
+
